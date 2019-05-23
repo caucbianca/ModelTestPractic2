f9b87dd56b497e28c16a1d5ee8438f79140fdbf6
Bianca-Mihaela Cauc <cauc.bianca@gmail.com>
2019-05-23 14:50:48 +0300

    message

---
+++ app/src/main/java/ro/pub/cs/systems/eim/main/MainActivity.java
@@ -0,0 +1,72 @@
+package ro.pub.cs.systems.eim.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ro.pub.cs.systems.pdsd.practicaltest02.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView defin;
+    EditText serverPort, clientPort, address, city;
+    Button connect, get;
+
+    ServerThread serverThread = null;
+    ClientThread clientThread = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        serverPort = (EditText) findViewById(R.id.serverPort);
+        clientPort = findViewById(R.id.clientPort);
+        address = findViewById(R.id.address);
+        city = findViewById(R.id.city);
+        connect = findViewById(R.id.connect);
+        get = findViewById(R.id.weather);
+        defin = findViewById(R.id.weatherView);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String svPort = serverPort.getText().toString();
+                if (svPort != null && !svPort.isEmpty()) {
+                    serverThread = new ServerThread(Integer.parseInt(svPort));
+                    serverThread.start();
+                }
+            }
+        });
+
+        get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = city.getText().toString();
+
+                String clPort = clientPort.getText().toString();
+                String clAddress = address.getText().toString();
+
+                clientThread = new ClientThread(clAddress, Integer.parseInt(clPort), query, defin);
+                clientThread.start();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        if (serverThread != null)
+            serverThread.stopThread();
+        super.onDestroy();
+
+
+    }
+}
---
+++ app/src/main/java/ro/pub/cs/systems/eim/main/OwnContainer.java
@@ -0,0 +1,10 @@
+package ro.pub.cs.systems.eim.main;
+
+public class OwnContainer {
+    public String queryResponse1;
+
+    public OwnContainer(String queryResponse1)
+    {
+        this.queryResponse1 = queryResponse1;
+    }
+}
